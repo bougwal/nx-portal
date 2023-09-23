c414c36a89b588d1a90bb4eab7e7bf2bd7bb34d2
Walid Bouguima <bougwal@hilti.com>
2023-09-23 20:56:45 +0200

    feat:dispatch initialization event via store in poiList cmp

--- libs/visitor/src/lib/poi-list/poi-list.component.ts
+++ libs/visitor/src/lib/poi-list/poi-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PoiActions } from '@packt/poi';
 
 @Component({
   selector: 'packt-poi-list',
   templateUrl: './poi-list.component.html',
   styleUrls: ['./poi-list.component.css']
 })
-export class PoiListComponent {
+export class PoiListComponent implements OnInit{
+
+  private readonly store = inject(Store); 
+
+  ngOnInit(): void {
+      this.store.dispatch(PoiActions.initPoi())
+  }
 
 }
