7f70612f78bef282f3de7d57d9d2a9616bc6b46a
Walid Bouguima <bougwal@hilti.com>
2023-09-23 15:24:07 +0200

    feat:enable router-outlet

--- libs/visitor/src/lib/visitor.module.ts
+++ libs/visitor/src/lib/visitor.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { VisitorComponent } from './visitor.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    MatToolbarModule, 
+    MatButtonModule,
+    MatSidenavModule, 
+    MatIconModule, 
+    MatListModule,
+    RouterModule.forChild([{ path: '', component: VisitorComponent }])],
+    declarations: [VisitorComponent], 
+    exports: [VisitorComponent]
 })
 export class LibsVisitorModule {}
