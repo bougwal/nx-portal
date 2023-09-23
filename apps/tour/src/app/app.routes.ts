22d52253fb5d03274a47a3262dfd1b06df6f8d4c
Walid Bouguima <bougwal@hilti.com>
2023-09-23 15:16:52 +0200

    core:set up default routing

--- apps/tour/src/app/app.routes.ts
+++ apps/tour/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'tour', 
+        loadChildren: ()=> import('@packt/libs/visitor').then(m => m.LibsVisitorModule)
+    }, 
+    {
+        path: '', 
+        pathMatch: 'full', 
+        redirectTo: 'tour'
+    }
+];
