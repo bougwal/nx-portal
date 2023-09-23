e83cebcbbf9ebff68edb33b73d00479d17bc072a
Walid Bouguima <bougwal@hilti.com>
2023-09-23 21:32:33 +0200

    feat: add the select poi by id to action and reducer state

--- libs/poi/src/lib/+state/poi.actions.ts
+++ libs/poi/src/lib/+state/poi.actions.ts
@@ -12,3 +12,5 @@ export const loadPoiFailure = createAction(
   '[Poi/API] Load Poi Failure',
   props<{ error: any }>()
 );
+
+export const selectPoi = createAction('[Poi/API] Select Poi', props<{poiId: string | number}>())
